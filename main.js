5d163908c495ba4c1950c7c3d489e122427f8abc
Shriyans <77096197+ShriyansGitHub@users.noreply.github.com>
2021-01-26 18:33:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,17 @@
 StudentArray1 = [];
 
-function Submit(){
-    var name1 = document.getElementById("Nameofstudent1").value;
-    var name2 = document.getElementById("Nameofstudent2").value;
-    var name3 = document.getElementById("Nameofstudent3").value;
-    var name4 = document.getElementById("Nameofstudent4").value;
-    var name5 = document.getElementById("Nameofstudent5").value;
-    StudentArray1.push(name1);
-    StudentArray1.push(name2);
-    StudentArray1.push(name3);
-    StudentArray1.push(name4);
-    StudentArray1.push(name5);
-    console.log(StudentArray1);
+function Submit() {
+    for (var j = 1; j <= 5; j++) {
+        var name_of_the_student = document.getElementById("Nameofstudent" + j).value;
+        console.log(name_of_the_student);
+        StudentArray1.push(name_of_the_student);
+    }
     document.getElementById("display_name").innerHTML = StudentArray1;
     document.getElementById("submitbtn").style.display = "none";
     document.getElementById("sortbtn").style.display = "inline-block";
 }
-function Sorting(){
+
+function Sorting() {
     StudentArray1.sort();
     document.getElementById("display_name").innerHTML = StudentArray1;
 }
